83b3f9f7d373187dce5b99c74e7000f2127fd986
lucasituniverse <lucas.manhaes@ituniverse.com.br>
2025-06-02 11:00:59 -0300

    Alterando formulario de pdv

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Formulario from "./page/Formulario";
+import Formulario from "./page/pdv";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./page/Home";
 import { Toaster } from "sonner";
